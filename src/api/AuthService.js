f724c55e0054c5d627a2e62e8f0453b0e2d46a98
TomyLingga <tomy.troller@gmail.com>
2024-07-30 11:49:38 +0700

    big update

--- src/api/AuthService.js
+++
@@ -1,9 +0,0 @@
-import http from "./http-common";
-
-class AuthService{
-    postSignIn(data){
-        return http.post('login',data);
-    }
-}
-
-export default new AuthService;
--- src/api/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { URL_API } from "./DataVariable";
-
-export default axios.create({
-    baseURL:URL_API,
-    headers:{
-        'Content-Type': 'application/json'
-    }
-})
